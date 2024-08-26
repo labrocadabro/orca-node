6f75b4efd16e3aa91df3bc7ccd9340ff52eb505d
Laura Abro <laura@koii.network>
2024-08-26 09:54:21 -0300

    prettier

--- dist/app.module.js
+++ dist/app.module.js
@@ -1,46 +1,62 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+'use strict';
+var __decorate =
+  (this && this.__decorate) ||
+  function (decorators, target, key, desc) {
+    var c = arguments.length,
+      r =
+        c < 3
+          ? target
+          : desc === null
+          ? (desc = Object.getOwnPropertyDescriptor(target, key))
+          : desc,
+      d;
+    if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
+      r = Reflect.decorate(decorators, target, key, desc);
+    else
+      for (var i = decorators.length - 1; i >= 0; i--)
+        if ((d = decorators[i]))
+          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.AppModule = void 0;
-const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const container_lifecycle_manager_service_1 = require("./container-lifecycle-manager/container-lifecycle-manager.service");
-const socketio_manager_service_1 = require("./socketio-manager/socketio-manager.service");
-const config_1 = require("@nestjs/config");
-const configuration_1 = require("./config/configuration");
-const app_service_1 = require("./app.service");
-const logger_module_1 = require("./logger/logger.module");
-const socketio_manager_module_1 = require("./socketio-manager/socketio-manager.module");
-const podman_install_service_1 = require("./podman-install/podman-install.service");
-const metrics_service_1 = require("./metrics/metrics.service");
-let AppModule = class AppModule {
-};
+const common_1 = require('@nestjs/common');
+const app_controller_1 = require('./app.controller');
+const container_lifecycle_manager_service_1 = require('./container-lifecycle-manager/container-lifecycle-manager.service');
+const socketio_manager_service_1 = require('./socketio-manager/socketio-manager.service');
+const config_1 = require('@nestjs/config');
+const configuration_1 = require('./config/configuration');
+const app_service_1 = require('./app.service');
+const logger_module_1 = require('./logger/logger.module');
+const socketio_manager_module_1 = require('./socketio-manager/socketio-manager.module');
+const podman_install_service_1 = require('./podman-install/podman-install.service');
+const metrics_service_1 = require('./metrics/metrics.service');
+let AppModule = class AppModule {};
 exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
+exports.AppModule = AppModule = __decorate(
+  [
     (0, common_1.Module)({
-        imports: [
-            config_1.ConfigModule.forRoot({
-                load: [configuration_1.default],
-                isGlobal: true,
-                ignoreEnvFile: true,
-            }),
-            logger_module_1.LoggerModule,
-            socketio_manager_module_1.SocketioManagerModule,
-        ],
-        controllers: [app_controller_1.AppController],
-        providers: [
-            config_1.ConfigService,
-            container_lifecycle_manager_service_1.ContainerLifecycleManagerService,
-            socketio_manager_service_1.SocketioManagerService,
-            podman_install_service_1.WinInstaller, podman_install_service_1.MacOSInstaller,
-            app_service_1.default,
-            metrics_service_1.MetricsService,
-        ],
-    })
-], AppModule);
+      imports: [
+        config_1.ConfigModule.forRoot({
+          load: [configuration_1.default],
+          isGlobal: true,
+          ignoreEnvFile: true,
+        }),
+        logger_module_1.LoggerModule,
+        socketio_manager_module_1.SocketioManagerModule,
+      ],
+      controllers: [app_controller_1.AppController],
+      providers: [
+        config_1.ConfigService,
+        container_lifecycle_manager_service_1.ContainerLifecycleManagerService,
+        socketio_manager_service_1.SocketioManagerService,
+        podman_install_service_1.WinInstaller,
+        podman_install_service_1.MacOSInstaller,
+        app_service_1.default,
+        metrics_service_1.MetricsService,
+      ],
+    }),
+  ],
+  AppModule,
+);
 //# sourceMappingURL=app.module.js.map
--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.bootstrap = void 0;
-const core_1 = require("@nestjs/core");
-const app_module_1 = require("./app.module");
-const app_service_1 = require("./app.service");
-const logger_service_1 = require("./logger/logger.service");
+const core_1 = require('@nestjs/core');
+const app_module_1 = require('./app.module');
+const app_service_1 = require('./app.service');
+const logger_service_1 = require('./logger/logger.service');
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
-        bufferLogs: true,
-    });
-    app.useLogger(app.get(logger_service_1.LoggerService));
-    await app.listen(3000);
-    return app.get(app_service_1.default);
+  const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(logger_service_1.LoggerService));
+  await app.listen(3000);
+  return app.get(app_service_1.default);
 }
 exports.bootstrap = bootstrap;
 //# sourceMappingURL=main.js.map
