606be5e931a29c7b86ff2f5ec46f7a5c1635d471
Laura Abro <laura@koii.network>
2025-02-04 12:44:06 -0400

    refactor

--- dist/config/configuration.js
+++ dist/config/configuration.js
@@ -1,22 +1,18 @@
 'use strict';
+const { execSync } = require('child_process');
+
 Object.defineProperty(exports, '__esModule', { value: true });
-const getWslGateway = () => {
-  if (process.platform === 'win32') {
+function getWslGateway() {
+  // Get gateway IP from the virtual machine
+  const output = execSync(`wsl -d podman-orca ip route`).toString();
+  const defaultRoute = output.split('\n').find(line => line.startsWith('default'));
+  if (defaultRoute) {
+    return defaultRoute.split(' ')[2].trim();
+  }
 
-      const { execSync } = require('child_process');
-      // Get current Podman machine name
-      const machineOutput = execSync('podman machine ls --format "{{.Name}}"').toString().trim().replace('*', '');
-      // Get gateway IP from that machine
-      const output = execSync(`wsl -d podman-${machineOutput} ip route`).toString();
-      const defaultRoute = output.split('\n').find(line => line.startsWith('default'));
-      if (defaultRoute) {
-        return defaultRoute.split(' ')[2].trim();
-      }
-    }
-    return 'host.containers.internal';
-};
+  return 'host.containers.internal';
+}
 
-console.log(getWslGateway());
 exports.default = () => ({
   serverPort: parseInt(process.env.ORCA_SERVER_PORT, 10) || 3003,
   serverIp: process.env.ORCA_SERVER_IP || '0.0.0.0',
