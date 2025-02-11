1c3ff8af52a11f65b69f992b2f0962438b8a2f98
Laura Abro <laura@koii.network>
2025-02-11 14:45:14 -0400

    try to fix multi line logging

--- dist/logger/logger.service.js
+++ dist/logger/logger.service.js
@@ -27,25 +27,27 @@ let LoggerService = class LoggerService extends common_1.ConsoleLogger {
     this.errorHandler = null;
     this.warnHandler = null;
     this.logHandler = null;
+    this.setupFormatters();
   }
-  log(message) {
-    if (this.logHandler != null) {
-      this.logHandler(message);
-    }
-    super.log(message);
+  setupFormatters() {
+    const _this = this;
+    // Log method
+    const originalLog = this.log.bind(this);
+    this.log = function (message, context) {
+      message.split('\n').forEach(line => originalLog(line, context));
+    };
+    // Error method
+    const originalError = this.error.bind(this);
+    this.error = function (message, stack, context) {
+      message.split('\n').forEach(line => originalError(line, stack, context));
+    };
+    // Warn method
+    const originalWarn = this.warn.bind(this);
+    this.warn = function (message, context) {
+      message.split('\n').forEach(line => originalWarn(line, context));
+    };
   }
-  error(message, stack, context) {
-    if (this.errorHandler != null) {
-      this.errorHandler(message, stack, context);
-    }
-    super.error(message, stack, context);
-  }
-  warn(message, context, ...rest) {
-    if (this.warnHandler != null) {
-      this.warnHandler(message, context, ...rest);
-    }
-    super.warn(message, context, ...rest);
-  }
+
   setLogHandler(fn) {
     this.logHandler = fn;
   }
